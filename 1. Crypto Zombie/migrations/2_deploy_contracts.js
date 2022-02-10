82e8b73d6032e2c27838af871520fff1459e8f8f
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2022-02-10 20:11:21 +0900

    [ Crypto Zombie ] Add deploy script to migrate ZombieFactory contract in migration directory

--- 1. Crypto Zombie/migrations/2_deploy_contracts.js
+++ 1. Crypto Zombie/migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
+// artifacts.require() 메서드를 통해 컨트랙트의 정보를 획득한다.
 const Zombie = artifacts.require("Zombie");
 
 module.exports = function (deployer) {
+  /*
+  deployer.deploy() 메서드를 통해 해당 컨트랙트를 배포한다.
+  다시 말해 해당 파일을 통해 Migrations에 작성된 컨트랙트를 배포할 수 있다.
+  */
   deployer.deploy(Zombie);
 };
