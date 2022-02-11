f2b9fc42f6a633464c8e7ea6eeb77928546ed25b
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2022-02-11 17:10:40 +0900

    [ Crypto Zombie ] Solidity Data area with EVM(Ethereum Virtual Machine) - Stack, Storage, Memory, and CallData

--- 1. Crypto Zombie/contracts/Zombie.sol
+++ 1. Crypto Zombie/contracts/Zombie.sol
@@ -6,7 +6,7 @@
 굳이 작성하지 않아도 되는, 오류가 아닌 주의사항이지만 0.6.8 버전 이후 생긴 방식이기 때문에 작성하는 걸 추천한다.
 */
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9;
+pragma solidity >=0.5.0 <0.9.0;
 
 /*
 솔리디티 코드는 컨트랙트 안에 싸여 있다.
@@ -59,8 +59,14 @@ contract ZombieFactory {
     이벤트는 애플리케이션의 사용자 단에서 어떤 행동이 발생했을 때 컨트랙트가 블록체인 상에서 이와 의사소통하는 방법이다.
     컨트랙트는 특정 이벤트가 일어나는 지 확인하고 해당 이벤트가 발생하면 행동을 취한다.
 
-    event 키워드를 사용하여 이벤트를 만들 수 있다.
-    해당 이벤트는 자바스크립트에서의 이벤트리스너와 유사하다고 생각하면 된다.
+    event 키워드를 사용하여 이벤트를 만들 수 있다. 해당 이벤트는 자바스크립트에서의 이벤트리스너와 유사하다고 생각하면 된다. 이때 중요한 점은 본문을 구현하지 않아야 한다는 것이다.
+
+    프론트엔드 코드에서는 Socket 이벤트를 초리하는 것처럼 해당 이벤트에 대한 콜백 코드를 작성하면 된다.
+    예를 들어 아래와 같은 NewZombie() 이벤트는 프론트엔드 상에서 다음과 같이 처리할 수 있다.
+
+    Contract.NewZombie((error, response) => {
+        console.log(response); // > response 출력
+    })
     */
     event NewZombie(uint zombieId, string name, uint dna);
 
@@ -75,12 +81,42 @@ contract ZombieFactory {
 
     솔리디티에는 storage와 memory 개념이 존재한다. 이는 변수를 저장할 수 있는 공간을 의미한다.
     storage의 경우 블록체인 상에서 영구 저장되는 개념이며 memory의 경우 임시적으로 저장되어서 호출이 발생할 때마다 초기화된다.
-
     구조체와 배열을 선언할 때 명시적으로 선언해야 해서 아래 string 자료형의 _name이라는 변수에 memory 키워드를 사용했다.
+    아래는 공식 문서에 나온 EVM(Ethereum Virtual Machine)의 데이터 저장 공간 내용이다.
+
+    The Ethereum Virtual Machine has three areas where it can store data - storage, memory and the stack, which are explained in the following paragraphs.
+
+    Each account has a data area called storage, which is persistent between function calls and transactions.
+    Storage is a key-value store that maps 256-bit words to 2560bit words.
+    It is not possible to enumerate storage from within a contract, it is comparatively costly to read, and even more to initialise and modify storage.
+    Because of this cose, you should minimize what you store in persistent storage to what the contract needs to run.
+    Store data like derived calculations, caching, and aggregates outside of the contract.
+    A contract can neither nor write to any storage apart from its own.
+
+    The second data is called memory, of which a contract obtains a freshly cleared instance for each message call.
+    Memory is linear and can be addressed at byte level, but reads are limited to a width of 256 bits, while writes can be either 8 bits or 256 bits wide.
+    Memory is expanded by a word (256-bit), when accessing (either reading or writing) a previously untouched memory word (i.e. any offset within a word).
+    At the time of expansion, the cost in gas must be paid. Memory is more costly the larger it grows (it scales quadratically).
+
+    The EVM is not a register machine but a stack machine, so all computations are performed on a data area called the stack.
+    It has a maximum size of 1024 elements and contains words of 256 bits.
+    Access to the stack is limited to the top end in the following way: It is posiible to copy one of the tompost 16 elements below it.
+    All other operations take the topmost two (or one, or more, depending on the operation) elements from the stack and push the result onto the stack.
+    Of course it is possible to move stack elements to storage or memory in order to get deeper access to the stack,
+    but it is not possible to just access arbitrary elements deeper in the stack without first removing the top of the stack.
+    Of course it is possible to move stack elements to storage or memory in order to get deeper access to the stack,
+    but it is not possible to just access arbitrary elements deeper in the stack without first removing the top of the stack.
+
+    요약하자면 다음과 같다.
+    Storage의 경우 함수 호출과 트랜잭션 사이에서 영구적인 데이터 영역이다.
+    Memory의 경우 각 메세지 호출 때마다 컨트랙트가 얻게 되는 새로운 인스턴스다.
+    Stack은 EVM 자체가 스택 기반의 머신이기 때문에 내부적으로 처음 올라가는 메모리다.
+
+    이외에도 Calldata 종류가 존재하는데 External Function의 매개변수로 사용된다.
     */
     function _createZombie(string memory _name, uint _dna) private {
         zombies.push(Zombie(_dna, _name));
-        // 기존에는 push() 메서드의 반환값이 해당 배열의 길이였지만 이제는 길이를 반환하지 않기 때문에 별도의 length 메서드를 따로 써줘야 한다.
+        // 기존에는 push() 메서드의 반환값이 해당 배열의 길이였지만 0.6.0 버전 이후 길이를 반환하지 않기 때문에 별도의 length 메서드를 따로 써줘야 한다.
         uint id = zombies.length - 1;
         /*
         emit 키워드를 사용하여 명시적으로 이벤트를 발생시키는 걸 알려준다.
@@ -93,6 +129,16 @@ contract ZombieFactory {
     함수에 반환값을 지정해주기 위해서는 returns 키워드를 사용해야 한다. 이때 함수 제어자의 종류로는 크게 view와 pure가 있다.
     view 함수의 경우 함수가 데이터를 보기만 하고 변경하지 않는다는 의미다.
     pure 함수의 경우 함수가 애플리케이션 내의 어떤 데이터도 접근하지 않는 것을 의미한다.
+
+    또한 require 조건문을 통해서 참인 경우에만 함수를 실행할 수 있다.
+    예를 들어 아래 코드에 require(_str == "Test"); 라는 구문을 추가할 경우 해당 함수의 매개변수로 전달 받은 _str의 값이 Test일 때만 함수가 실행된다.
+
+    그리고 값을 반환할 때 return (10, 20, 30); 과 같이 여러 값을 한 번에 반환할 수 있는데 이때 아래와 같이 원하는 값만 전달 받을 수 있다.
+    
+    uint number;
+    (, , number) = func();
+
+    이때 number의 값은 반환되는 (10, 20, 30) 중 세 번째 요소를 전달 받기 때문에 30이 된다.
     */
     function _generateRandomDna(string memory _str) private view returns(uint) {
         /*
@@ -101,6 +147,10 @@ contract ZombieFactory {
         블로체인에서 안전한 의사 난수를 만드는 방법은 매우 어려운 문제 중 하나로 항상 주의해야 한다.
 
         이때 keccak256 내장 해시 함수는 인자로 bytes를 받아야 하기 때문에 abi.encode 메서드를 활용하여 string 자료형을 bytes로 변환하였다.
+
+        abi.encode 외에도 abi.encodePAcked, abi.encodeWithSignature, abi.encodeWithSelector와 같이 3개의 종류가 더 존재한다. 각각의 차이점은 아래와 같다.
+        
+
         */
         uint rand = uint(keccak256(abi.encode(_str)));
         return rand % dnaModulus;
