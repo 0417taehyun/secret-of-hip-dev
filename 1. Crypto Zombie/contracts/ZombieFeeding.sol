876e17b7b606e3cef83fbd5563486ec3f86cd55e
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2022-02-13 01:04:11 +0900

    [ Crypto Zombie ] Multiple inheritance and Fix description of Function access modifier public, private, external, internal

--- 1. Crypto Zombie/contracts/ZombieFeeding.sol
+++ 1. Crypto Zombie/contracts/ZombieFeeding.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity > 0.5.0 < 0.9.0;
+pragma solidity >= 0.5.0 < 0.9.0;
 
 // 아래와 같이 다른 솔리디티 파일을 임포트해서 사용할 수 있다.
 import "./ZombieFactory.sol";
@@ -13,6 +13,7 @@ import "./ZombieFactory.sol";
 
 먼저 추상 컨트랙트(abstract contract)의 경우 하나의 함수만이라도 추상 함수(Abstract Function)로 정의되어 있으면 추상 컨트랙트로 간주된다.
 이때 추상 컨트랙트를 만드는 방법으로는 크게 abstarct 키워드와 virtual 키워드를 사용하는 방법이 존재한다.
+(참고로 virtual 키워드의 경우 0.6.0 버전 이후 등장한 문법이다.)
 
 먼저 abstract 키워드를 사용하는 경우 contract 키워드 대신 사용하여 해당 컨트랙트 내부의 모든 걸 추상화시킨다.
 따라서 아래 ChildA 컨트랙트처럼 abstract 키워드를 사용한 추상 컨트랙트 ParentA를 상속 받을 경우 내부 함수인 testA()의 본문을 무조건 상속 받아 구현해야 한다.
@@ -41,6 +42,7 @@ contract ChildB is ParentB {}
 다시 말해 부모 컨트랙트에 동일하게 필요한 함수를 정의한 뒤 이를 종속 받은 자식 컨트랙트가 필요한 방식으로 확장, 변화하여 구현하여 코드 중복을 최소화하는 패턴을 의미한다.
 
 interface 키워드를 사용하여 구현할 경우 본문 내용이 존재하는 함수, 다시 말해 구현이 완료된 함수를 절대 포함할 수 없다.
+또한 내부 함수의 경우 필히 external 키워드를 사용해야 한다.
 
 abstract 키워드를 사용할 경우 반대로 구현이 완료된 함수도 포함될 수 있다.
 그 예시는 아래 컨트랙트의 testD() 함수와 같다.
@@ -49,8 +51,25 @@ abstract contract ParentC {
     function testC() public virtual returns (uint);
     function testD() public pure returns (uint) { return 2; }
 }
-*/
 
+추가적으로 private 함수는 virtual 함수가 될 수 없다.
+또한 인터페이스 외부의 구현되지 않은 함수에는 virtual 키워드를 써야 하며 인터페이스 내부의 함수는 자동으로 virtual 함수로 고려된다.
+인터페이스 함수를 상속 받아서 override 할 때 0.8.8 버전 이후부터는 다중 상속을 받는 경우가 아닌 이상 더이상 override 키워드를 사용하지 않아도 된다.
+
+솔리디티에서 다중 상속은 아래와 같이 구현할 수 있다.
+
+constract Child is Mother, Father {}
+
+이때 특이한 점은 파이썬과 달리 우측에서 좌측으로 상속의 순서가 결정되어 Father를 먼저 상속 받게 된다.
+따라서 그 순서는 Child -> Father -> Mother 컨트랙트와 같다.
+
+이때 만약 Father 컨트랙트와 Mother 컨트랙트 모두 Grand 컨트랙트를 상속 받는다면 Grand 컨트랙트는 제일 뒤로 순서가 밀리게 된다.
+따라서 그 순서는 Child -> Father -> Mother -> Grand 컨트랙트와 같다.
+솔리디티는 내부적으로 Father 컨트랙트가 Grand 컨트랙트를 상속 받을 경우 해당 Grand 컨트랙트가 또다른 컨트랙트에 부모가 되지 않는 지 먼저 확인한다.
+그리고 만약 다른 컨트랙트에서 - 위 경우에서는 Mother 컨트랙트 - Grand 컨트랙트를 상속 받을 경우 그 순서를 제일 뒤로 미룬다.
+
+솔리디티는 이러한 방식으로 다중 상속이 일으킬 수 있는 다이아몬드 문제를 예방할 수 있고 또한 C3 선형화 알고리즘(linerization Algorithm)을 채택하고 있다.
+*/
 interface KittyInterface {
     function getKitty(uint256 _id) external view returns (
         bool isGestating,
