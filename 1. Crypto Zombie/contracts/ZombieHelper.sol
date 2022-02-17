091c330551d0379faccacaa713162d4966538667
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2022-02-17 20:37:35 +0900

    [ Crypto Zombie ] Zombie Battle System: address payable with transfer or send method and this keword with address

--- 1. Crypto Zombie/contracts/ZombieHelper.sol
+++ 1. Crypto Zombie/contracts/ZombieHelper.sol
@@ -7,9 +7,11 @@ contract ZombieHelper is ZombieFeeding {
 
     /*
     이더(Ehter)의 경우 아래와 같이 ether 키워드를 붙인다.
-    이때 아래와 같이 constant 키워드를 붙여 해당 변수를 상수로 만들 수 있다.
-    */
+    만약 해당 변수를 상수로 만들고 싶다면 아래와 같이 constant 키워드를 붙이면 된다.
+
     uint public constant levelUpFee = 0.001 ether;
+    */
+    uint levelUpFee = 0.001 ether;
     
     // modifier에도 매개변수를 전달할 수 있다.
     modifier aboveLevel(uint _level, uint _zombieId) {
@@ -20,10 +22,43 @@ contract ZombieHelper is ZombieFeeding {
     /*
     이더리움 위에는 트랜잭션 데이터는 물론 컨트랙트 코드와 이더(Ether) 모두 존재하기 때문에 payable 키워드를 통해
     해당 함수가 일정량의 이더를 지불하게 만들 수 있다.
+
+    이때 유의할 점은 msg.value의 경우 internal 또는 payable 함수에서만 사용 가능하다.
     */
     function levelUp(uint _zombieId) external payable {
         require(msg.value == levelUpFee);
         zombies[_zombieId].level++;
+    }
+
+    /*
+    address(this)와 msg.sender는 둘 다 주소를 반환하지만 차이가 있다.
+    address(this)의 경우 함수 호출이 발생한 해당 컨트랙트의 인스턴스의 주소를 반환한다.
+    msg.sender의 경우 컨트랙트가 호출된 주소를 반환한다.
+
+    따라서 아래 withdraw() 함수의 경우 해당 컨트랙트를 호출한 사람(msg.sender)이
+    해당 컨트랙트의 인스턴스(address(this))의 통장(.balance)으로 비용을 지출한다고 생각하면 편하다.
+
+    솔리디티 0.8 버전 이상부터는 payable 키워드 앞에 명시적으로 address 키워드를 붙여주지 않아도 된다.
+
+    이때 address 타입과 address payable 타입에는 차이점이 존재한다.
+    address의 경우 transfer, send 등의 이더(Ehter) 비용을 다룰 수 있는 메서드를 사용할 수 없다.
+    반대로 address payalbe의 경우 사용이 가능하기 때문에 단순 address 타입을 payable로 변환하기 위해서는 명시적 형변환이 필요하다.
+
+    address payable _owner = payable(address(owner()));
+
+    그리고 해당 _owner 변수를 사용하여 아래와 같이 transfer 메서드를 통해 이더(Ehter) 비용 전송이 가능하다.
+
+    _owner.transfer(address(this).balance);
+
+    추가적으로 msg.send와 transfer 메서드는 동일한 역할을 수행한다.
+    이때 sent의 경우 로우레벨 호출이기 때문에 오류가 발생했을 때 오류가 났을 때 예외처리가 가능하다.
+    */
+    function withdraw() external onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }
+
+    function setLevelUpFee(uint _fee) external onlyOwner {
+        levelUpFee = _fee;
     }
 
     // calldata는 memory와 유사하지만 external 함수에서만 가능하다.
