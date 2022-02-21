5d85d70ddb501daa7631423f630a0dbaa90bf1f9
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2022-02-21 22:07:32 +0900

    [ Crypto Zombie ] ERC721

--- 1. Crypto Zombie/contracts/ERC721.sol
+++ 1. Crypto Zombie/contracts/ERC721.sol
@@ -5,5 +5,21 @@ import "./Context.sol";
 import "./IERC721.sol";
 
 contract ERC721 is Context, IERC721 {
-    
+    // Token name
+    string private _name;
+
+    // Token symbol
+    string private _symbol;
+
+    // Mapping from token ID to owner address
+    mapping(uint256 => address) private _owners;
+
+    // Mapping owner address to token count
+    mapping(address => uint256) private _balances;
+
+    // Mapping from token ID to approved address
+    mapping(uint256 => address) private _tokenApprovals;
+
+    // Mapping from owner to operator approvals
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
 }
