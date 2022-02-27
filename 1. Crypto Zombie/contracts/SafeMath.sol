e09dcb878b1c2d74ae695b988ad9bcb0401f2197
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2022-02-27 22:57:09 +0900

    [ Crypto Zombie ] ERC721 & Crypto-Collectibles: NatSpec Format for comment, diffirence of assert and require

--- 1. Crypto Zombie/contracts/SafeMath.sol
+++ 1. Crypto Zombie/contracts/SafeMath.sol
@@ -2,5 +2,40 @@
 pragma solidity ^0.8.0;
 
 library SafeMath {
-    
+
+    /*
+    산술작업은 언더플로우와 오버플로우 상황에서 되돌릴 수 있는데,
+    unchecked를 사용하여 이전에 수행하던 확인 부분을 감싸 표현할 수 있다.
+
+    솔리디티는 0.8 버전 이후부터 내부적으로 자체 언더플로우, 오버플로우 확인 작업을 해주고
+    오류가 발생할 경우 이를 되돌려준다.
+
+    결국 이전에는 내부적으로 구현이 안 되어 있어 라이브러리 SafeMath를 만들고 구현하던 것들을
+    사용할 필요가 없다는 의미인데 혹시 사용하고 싶다면 unchecked 키워드를 통해 사용할 수 있다는 의미다.
+
+    추가적으로 기존의 if 구문 등을 통해서 직접 작업하는 것에 비해 unchecked를 사용할 경우 가스 비용을 절약할 수 있다.
+
+    require과 비슷한 역할을 하는 키워드로 assert가 존재한다.
+    require의 경우 만약 조건을 충족하지 못했을 때 가스 비용을 사용자에게 반환해주지만
+    assert의 경우 문제가 발생해도 가스 비용을 반환해주지 않는다.
+
+    따라서 대부분의 조건에 대해서는 require을 사용하는 게 좋으며
+    언더 및 오버플로우와 같은 매우 심각한 문제가 발생하는 경우를 확인할 때만 사용하는 게 좋다.
+    */
+    function tryAdd(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            uint256 c = a + b;
+            if (c < a) return (false, 0);
+            return (true, c);
+        }
+    }
+
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a + b;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a - b;
+    }
 }
--- 1. Crypto Zombie/contracts/ZombieOwnership.sol
+++ 1. Crypto Zombie/contracts/ZombieOwnership.sol
@@ -7,17 +7,20 @@ import "./ZombieAttack.sol";
 
 
 /*
+솔리디티에서 주석을 남기는 방식은 natspec을 따른다.
+아래 공식 문서를 확인하면 편하다.
+https://docs.soliditylang.org/en/latest/natspec-format.html
+
 원래 ERC20 토큰을 사용하여 transferFrom 등의 정형화된 메서드를 사용한다.
 그러나 해당 토큰은 0.027과 같이 세부적으로 나뉠 수 있는데 만약 좀비와 같이 하나의 유닛으로 교환하고 해당 유닛이 상대적으로 값이 다른 경우 다른 토큰 정책이 필요하다.
 보통 이럴 때 사용하는 게 ERC721이다.
 
 이떼 ERC는 Ethereum Request for Comment의 약자로 RFC(Request For Comment)의 경우 인터넷 기술에 대해 새로운 기술을 제시하고 비평을 받기 기다리는 문서를 뜻한다.
 다시 말해 ERC는 이더리움(Ethereum)과 관련된 새로운 기술을 제시하는 문서이다. 그리고 다른 사람의 의견에 따라 표준이 될 수도 있다.
-
 */
-
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
+    // 라이브러리는 다음과 같이 선언하여 사용할 수 있다.
     using SafeMath for uint256;
 
     mapping (uint => address) zombieApprovals;
@@ -33,8 +36,13 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
     }
 
     function _transfer(address _from, address _to, uint256 _tokenId) private {
-        ownerZombieCount[_from]--;
-        ownerZombieCount[_to]++;
+
+        /*
+        이때 using을 사용한 선언 방식을 통해 아래와 같이 owenrZombieCount의 값을
+        자동적으로 함수의 첫 번째 인자로 전달해줄 수 있다.
+        */
+        ownerZombieCount[_to] = ownerZombieCount[_to].add(1);
+        ownerZombieCount[_from] = ownerZombieCount[_from].sub(1);
         zombieToOwner[_tokenId] = _to;
         emit Transfer(_from, _to, _tokenId);
     }
